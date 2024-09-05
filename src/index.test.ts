fb4c6c49b7559cb03bf3dfabab7117d671a5a1a0
21aslade <andrewmslade@gmail.com>
2024-09-04 21:14:40 -0600

    add uint parser

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "@jest/globals";
-import { tag } from ".";
+import { tag, uint } from ".";
 import { Result } from "typescript-result";
 
 describe("tag", () => {
@@ -11,5 +11,31 @@ describe("tag", () => {
     it("doesn't match mid-string", () => {
         const parser = tag("bcd");
         expect(parser("abcde")).toEqual(Result.error(new Set(["bcd"])));
+    })
+})
+
+describe("uint", () => {
+    it("matches positive", () => {
+        expect(uint("3420abc")).toEqual(Result.ok([3420, 4]));
+    })
+
+    it("doesn't include e", () => {
+        expect(uint("321e10")).toEqual(Result.ok([321, 3]));
+    })
+
+    it("doesn't include .", () => {
+        expect(uint("321.10")).toEqual(Result.ok([321, 3]));
+    })
+
+    it("doesn't match negative", () => {
+        expect(uint("-5")).toEqual(Result.error(new Set(["nonnegative integer"])));
+    })
+
+    it("doesn't match zero prefix", () => {
+        expect(uint("0123")).toEqual(Result.error(new Set(["nonnegative integer"])));
+    })
+
+    it("doesn't match whitespace", () => {
+        expect(uint(" 123")).toEqual(Result.error(new Set(["nonnegative integer"])));
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { Result } from "typescript-result";
 type PError = Set<string>;
 
 type PResult<T> = Result<[T, number], PError>;
+const PResult = {
+    expected(expected: string): PResult<never> {
+        return Result.error(new Set([expected]))
+    }
+}
 
 type Parser<T> = (s: string) => PResult<T>;
 
@@ -11,7 +16,35 @@ export function tag(tag: string): Parser<string> {
         if (s.startsWith(tag)) {
             return Result.ok([tag, tag.length]);
         } else {
-            return Result.error(new Set([tag]));
+            return PResult.expected(tag);
         }
     };
 }
+
+function uintPrefixLen(s: string): number {
+    if (s[0] === "0") { return 0; }
+
+    let numberLength = 0;
+    while (numberLength < s.length) {
+        const c = s[numberLength] ?? '';
+        if (c < "0" || c > "9") {
+            return numberLength;
+        }
+
+        numberLength++;
+    }
+
+    return numberLength;
+}
+
+export const uint: Parser<number> = (s: string) => {
+    const len = uintPrefixLen(s);
+
+    if (len === 0) {
+        return PResult.expected("nonnegative integer");
+    }
+
+    const numeric = s.slice(0, len);
+    const parsed = parseInt(numeric);
+    return Result.ok([parsed, len]);
+};
